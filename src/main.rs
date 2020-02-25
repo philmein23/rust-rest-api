9a783c577bc0935d7d4f38a37798f82444742079
Phillip Nguyen <c-phillip.nguyen@hulu.com>
2020-02-24 17:10:49 -0800

    WIP rest api

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#[macro_use]
+extern crate log;
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use listenfd::ListenFd;
+use std::env;
+
+mod user;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Derppppp")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new().configure(user::init_routes));
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("Host not set");
+            let port = env::var("PORT").expect("Port not set");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+
+    info!("Starting server");
+
+    server.run().await
+}
